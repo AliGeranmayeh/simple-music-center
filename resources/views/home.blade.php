86dfdc4e049f490f39948b58c50adbb5f40c3bb5
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-20 16:22:46 +0430

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        @foreach ($musics as $music)
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+            <div class="card" style="width: 18rem;">
+                <img src="{{asset('uploads/images/'.$music->image)}}" class="card-img-top" alt="Music Cover">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Music Name: {{$music->name}}</li>
+                  <li class="list-group-item">Music Artist: {{$music->artist}}</li>
+                  <li class="list-group-item"><audio controls>
+                    <source src="{{asset('uploads/musics/'.$music->music)}}" type="audio/mpeg"> 
+                    </audio>
+                </li>
+                <li class="list-group-item">Music Description: {{$music->description}}</li>
+                </ul>
             </div>
-        </div>
+      </div>
+</div>
+        @endforeach
+
     </div>
 </div>
 @endsection
