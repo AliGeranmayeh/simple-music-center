48bdf75656c30e4091425749f6b9bb0a58944ee8
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-20 15:53:02 +0430

    validate and create new music

--- app/Http/Controllers/AddMusicController.php
+++ app/Http/Controllers/AddMusicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Music;
 
 class AddMusicController extends Controller
 {
@@ -13,6 +14,29 @@ class AddMusicController extends Controller
 
     public function store(Request $request)
     {
+       $request->validate([
+           'name' => 'required|string',
+           'artist' => 'required|string',
+           'description' => 'required|string',
+           'image' => 'required|mimetypes:jpg,png,jpeg',
+           'music' => 'required|mimetypes:mp3,mpeg'
+       ]);
+
        
+
+       $imageName = time().'-'.$request->name.'.'.$request->image->extension();
+       $musicName =  time().'-'.$request->music.'.'.$request->music->extension();
+       $request->image->move(public_path('uploads/images'),$imageName);
+       $request->music->move(public_path('uploads/musics'),$musicName);
+
+       Music::create([
+        'name' => $request->name,
+        'artist' => $request->artist,
+        'description' => $request->description,
+        'image' => $request->image,
+        'music' => $request->music
+       ]);
+
+       return redirect()->route('home');
     }
 }
