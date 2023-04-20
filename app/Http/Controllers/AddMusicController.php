f0f97281344da31788dec4c27ccc6e8e2fa29242
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-20 15:33:46 +0430

    complete index function

--- app/Http/Controllers/AddMusicController.php
+++ app/Http/Controllers/AddMusicController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AddMusicController extends Controller
 {
-    //
+    public function index()
+    {
+        return view("addNewMusic");
+    }
+
+    public function store(Request $request)
+    {
+       
+    }
 }
