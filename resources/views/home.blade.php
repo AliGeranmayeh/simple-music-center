c3c01c845ffc50496a062e52b13f0ba4b29570fa
expensive77 <51305499+expensive77@users.noreply.github.com>
2023-04-20 16:50:00 +0430

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
-        @foreach ($musics as $music)
+        
         <div class="col-md-8">
-            <div class="card" style="width: 18rem;">
-                <img src="{{asset('uploads/images/'.$music->image)}}" class="card-img-top" alt="Music Cover">
+            <div class="row">
+            @foreach ($musics as $music)
+            <div class="card m-2" style="width: 18rem;">
+                <img src="{{asset($music->image)}}" class="card-img-top img-thumbnail" alt="Music Cover" >
                 <ul class="list-group list-group-flush">
                   <li class="list-group-item">Music Name: {{$music->name}}</li>
                   <li class="list-group-item">Music Artist: {{$music->artist}}</li>
-                  <li class="list-group-item"><audio controls>
-                    <source src="{{asset('uploads/musics/'.$music->music)}}" type="audio/mpeg"> 
+                  <li class="list-group-item"><audio  controls style="width: 90%;">
+                    <source  src="{{asset($music->music)}}" type="audio/mpeg" style="color:black"> 
                     </audio>
                 </li>
                 <li class="list-group-item">Music Description: {{$music->description}}</li>
                 </ul>
             </div>
+            @endforeach
+        </div>
       </div>
 </div>
-        @endforeach
 
     </div>
 </div>
